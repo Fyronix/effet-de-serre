2874ae8148a17d927ee6c6e0591098ae9e0b82cf
Fyronix <86478645+Fyronix@users.noreply.github.com>
2023-03-03 15:28:58 +0100

    Update tests.js

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -2,69 +2,28 @@ describe(`Unit test the landing page`, () => {
   it(`loads`, () => {
     cy.visit(`/`);
   });
+
   it(`has latest CO2 value`, () => {
-    assert(
-      cy
-        .get(`#latestCo2Value`)
-        .invoke(`text`)
-        .then(text => {
-          assert(!isNaN(text));
-        })
-    );
+    cy.get(`#latestCo2Value`).should('have.text').and('not.be.NaN');
   });
 
   it(`has latest temperature value`, () => {
-    assert(
-      cy
-        .get(`#latestTempValue`)
-        .invoke(`text`)
-        .then(text => {
-          assert(!isNaN(text));
-        })
-    );
+    cy.get(`#latestTempValue`).should('have.text').and('not.be.NaN');
   });
 
   it(`has latest CO2 year`, () => {
-    assert(
-      cy
-        .get(`#latestCo2Year`)
-        .invoke(`text`)
-        .then(text => {
-          assert(!isNaN(text));
-        })
-    );
+    cy.get(`#latestCo2Year`).should('have.text').and('not.be.NaN');
   });
 
   it(`has latest temperature year`, () => {
-    assert(
-      cy
-        .get(`#latestTempYear`)
-        .invoke(`text`)
-        .then(text => {
-          assert(!isNaN(text));
-        })
-    );
+    cy.get(`#latestTempYear`).should('have.text').and('not.be.NaN');
   });
 
   it(`has 10 year temp change`, () => {
-    assert(
-      cy
-        .get(`#tenYearWarming`)
-        .invoke(`text`)
-        .then(text => {
-          assert(!isNaN(text));
-        })
-    );
+    cy.get(`#tenYearWarming`).invoke('text').then(parseFloat).should('not.be.NaN');
   });
 
   it(`has stamp with text`, () => {
-    assert(
-      cy
-        .get(`#stamp`)
-        .invoke(`text`)
-        .then(text => {
-          assert(text);
-        })
-    );
+    cy.get(`#stamp`).should('have.text').and('not.be.empty');
   });
 });
